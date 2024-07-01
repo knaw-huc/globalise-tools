1f8974082112463d61c9a11c1ef95324d9159285
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2024-07-01 15:06:52 +0200

    add progressbar, keep track of processed inv_nrs

--- scripts/gt-extract-paragraph-text.py
+++ scripts/gt-extract-paragraph-text.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-import argparse
 import csv
 import glob
+import json
 import os
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 import pagexml.helper.pagexml_helper as pxh
 import pagexml.parser as px
 from loguru import logger
+from tqdm import tqdm
 
 import globalise_tools.tools as gt
 
@@ -17,26 +19,34 @@ class ParagraphTextExtractor:
     def __init__(self, input_dir: str, output_dir: str):
         self.input_dir = input_dir
         self.output_dir = output_dir
+        self.processed_file = f"{self.output_dir}/extract-paragraph-text-processed-inv-nrs.json"
+        self.processed_inv_nrs = self.load_processed_files()
 
     def extract_paragraph_text(self):
         inv_nrs = sorted(
             [p.split("/")[-1] for p in glob.glob(f"{self.input_dir}/*") if os.path.isdir(p)])
-        for i in inv_nrs:
-            self._process_inv(i)
+        progress_bar = tqdm(inv_nrs)
+        for inv_nr in progress_bar:
+            if inv_nr in self.processed_inv_nrs:
+                progress_bar.set_description(f"skipping {inv_nr}, already processed")
+            else:
+                self._process_inv(inv_nr, progress_bar)
+                self.processed_inv_nrs.add(inv_nr)
+                self.store_processed_files()
 
     def _pagexml_paths(self, inv_nr: str) -> list[str]:
         return sorted(glob.glob(f"{self.input_dir}/{inv_nr}/NL-HaNA_1.04.02_{inv_nr}_*.xml"))
 
-    def _process_inv(self, inv_nr: str):
+    def _process_inv(self, inv_nr: str, bar):
+
         file_name = f"{self.output_dir}/{inv_nr}-paragraphs.tsv"
-        logger.info(f"=> {file_name}")
+        bar.set_description(f"=> {file_name}")
         with open(file_name, mode='w', newline='') as file:
             writer = csv.writer(file, delimiter='\t')
             writer.writerow(["inv_nr", "page_no", "textarea_id", "paragraph_text"])
             for path in self._pagexml_paths(inv_nr):
                 parts = path.split('/')
                 page_no = parts[-1].split('_')[-1].replace('.xml', '')
-                # logger.info(f"<= {path}")
                 scan_doc = px.parse_pagexml_file(pagexml_file=path)
                 for tr in [tr for tr in scan_doc.get_text_regions_in_reading_order() if gt.is_paragraph(tr)]:
                     lines_with_text = [l for l in tr.lines if l.text]
@@ -45,12 +55,25 @@ class ParagraphTextExtractor:
                     if tr_text:
                         writer.writerow([inv_nr, page_no, tr.id, tr_text])
 
+    def load_processed_files(self):
+        if os.path.exists(self.processed_file):
+            logger.info(f"<= {self.processed_file}")
+            with open(self.processed_file) as f:
+                processed = set(json.load(f))
+        else:
+            processed = set()
+        return processed
+
+    def store_processed_files(self):
+        with open(self.processed_file, 'w') as f:
+            json.dump(list(self.processed_inv_nrs), fp=f)
+
 
 @logger.catch
 def get_arguments():
-    parser = argparse.ArgumentParser(
+    parser = ArgumentParser(
         description="Extract paragraph text from pagexml files",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i",
                         "--input-directory",
                         required=True,
