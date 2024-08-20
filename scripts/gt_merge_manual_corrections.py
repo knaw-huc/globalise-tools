83f11a60590fcb1917d5ee4d1704d5de3fe2c7f7
Maarten van Gompel <proycon@anaproy.nl>
2024-08-20 14:22:22 +0200

    language detection: renamed 'manual' column to 'corrected'

--- scripts/gt_merge_manual_corrections.py
+++ scripts/gt_merge_manual_corrections.py
@@ -31,16 +31,16 @@ def main():
                     fieldnames = list(row.keys())
                 key = (row['inv_nr'],row['page_no'])
                 if filename == args.inputfile:
-                    row['manual'] = 0
+                    row['corrected'] = 0
                 else:
-                    row['manual'] = 1
+                    row['corrected'] = 1
                     if key not in data and not args.all:
                         print(f"Warning: key from corrections not found in original data {args.inputfile}: ",key, file=sys.stderr)
                         continue
                 data[key] = row
     assert fieldnames is not None
-    if 'manual' not in fieldnames:
-        fieldnames.append('manual')
+    if 'corrected' not in fieldnames:
+        fieldnames.append('corrected')
     writer = csv.DictWriter(sys.stdout,fieldnames=fieldnames,delimiter="\t",quoting=csv.QUOTE_NONE,extrasaction='ignore',escapechar="\\")
     writer.writeheader()
     for key, row in data.items():
