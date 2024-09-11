cbe88a1e9be2de5050626cf63d0c9ef599826c14
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2024-09-11 16:33:53 +0200

    some uptimisations; store missing pages to file

--- scripts/gt-update-annotations.py
+++ scripts/gt-update-annotations.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import json
+
 import hydra
 from annorepo.client import AnnoRepoClient
 from loguru import logger
@@ -13,34 +15,41 @@ def main(cfg: DictConfig) -> None:
     lang_deduction_for_page = ld.read_lang_deduction_for_page(cfg.automated_page_langs_file)
     ar = AnnoRepoClient(cfg.annorepo.base_uri, api_key=cfg.annorepo.api_key)
     ca = ar.container_adapter(cfg.annorepo.container_name)
-    annotation_iterator = ca.read_annotations()
     pages_missing_in_lang_detection = []
-    for anno in annotation_iterator:
+    for anno in ca.read_annotations():
         if anno["body"]["type"] == "px:Page":
             anno_url = anno["id"]
             anno_name = anno_url.split("/")[-1]
-            anno_result = ca.read_annotation(anno_name)
-            etag = anno_result.etag
-            anno = anno_result.annotation
-
             page_id = anno["body"]["metadata"]["document"]
             if page_id in lang_deduction_for_page:
-                lang_deduction = lang_deduction_for_page[page_id]
-                # update the annotation dict
-                anno["body"]["metadata"]["lang"] = lang_deduction.langs
-                anno["body"]["metadata"]["langCorrected"] = lang_deduction.corrected
-                logger.info(f"updating annotation {anno_url}")
-                ca.update_annotation(anno_name, etag, anno)
+                if "lang" not in anno["body"]["metadata"]:
+                    anno_result = ca.read_annotation(anno_name)
+                    etag = anno_result.etag
+                    anno = anno_result.annotation
+                    lang_deduction = lang_deduction_for_page[page_id]
+                    # update the annotation dict
+                    anno["body"]["metadata"]["lang"] = lang_deduction.langs
+                    anno["body"]["metadata"]["langCorrected"] = lang_deduction.corrected
+                    logger.info(f"updating annotation {anno_url}")
+                    ca.update_annotation(anno_name, etag, anno)
             else:
                 logger.warning(f"no language detection found for page {page_id} / {anno['id']}")
                 pages_missing_in_lang_detection.append(page_id)
     if pages_missing_in_lang_detection:
-        print(f"Pages missing in {cfg.automated_page_langs_file} :")
-        for page_id in pages_missing_in_lang_detection:
-            parts = page_id.split('_')
-            inv_nr = parts[-2]
-            page_no = parts[-1]
-            print(f"{inv_nr}\t{page_no}")
+        path = "out/gt-update-annnotations-missing-lang-detection.json"
+        logger.info(f"=> {path}")
+        with open(path) as f:
+            json.dump(pages_missing_in_lang_detection, fp=f, indent=4)
+        # print_missing_lang_detection(cfg, pages_missing_in_lang_detection)
+
+
+def print_missing_lang_detection(cfg, pages_missing_in_lang_detection):
+    print(f"Pages missing in {cfg.automated_page_langs_file} :")
+    for page_id in pages_missing_in_lang_detection:
+        parts = page_id.split('_')
+        inv_nr = parts[-2]
+        page_no = parts[-1]
+        print(f"{inv_nr}\t{page_no}")
 
 
 if __name__ == '__main__':
