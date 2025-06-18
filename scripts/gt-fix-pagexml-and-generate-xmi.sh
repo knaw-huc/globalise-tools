f1f4ced394478fb6ec4a54317acad8e99e6ace86
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2025-06-18 18:00:56 +0200

    make gt-pagexml-to-uima-cas accept multiple pagexml paths

--- scripts/gt-fix-pagexml-and-generate-xmi.sh
+++ scripts/gt-fix-pagexml-and-generate-xmi.sh
@@ -2,6 +2,9 @@
 INV=$1
 INDIR=~/c/data/globalise/pagexml/$INV
 OUTDIR=out-local/fixed-pagexml/$INV
-md $OUTDIR
-cp $INDIR/*xml $OUTDIR
-poetry run ./scripts/gt_fix_reading_order2.py -i ~/c/data/globalise/pagexml -o $OUTDIR $INV
+mkdir -p $OUTDIR
+cp -a $INDIR/*xml $OUTDIR
+poetry run ./scripts/gt_fix_reading_order2.py -i out-local/fixed-pagexml -o $OUTDIR $INV
+poetry run ./scripts/gt-pagexml-to-uima-cas.py -o $OUTDIR $OUTDIR/*.xml
+chmod a-x $OUTDIR/*.xm?
+(cd out-local/fixed-pagexml && zip -qr $INV-pagexml.zip $INV/*.xml && zip -qr $INV-xmi.zip $INV/*.xmi)
--- scripts/gt-pagexml-to-uima-cas.py
+++ scripts/gt-pagexml-to-uima-cas.py
@@ -21,63 +21,70 @@ def get_arguments():
     parser = argparse.ArgumentParser(
         description="Convert a PageXML file to UAMI CAS XMI",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-o",
+                        "--output-directory",
+                        required=False,
+                        help="The directory to save the xmi file(s) to",
+                        type=str)
     parser.add_argument("page_xml_path",
+                        nargs='+',
                         help="The path to the pagexml file.",
                         type=str)
     return parser.parse_args()
 
 
-def output_path(page_xml_path: str) -> str:
+def output_path(page_xml_path: str, output_directory: str) -> str:
     base = page_xml_path.split("/")[-1].replace(".xml", "")
-    return f"out/{base}.xmi"
+    return f"{output_directory}/{base}.xmi"
 
 
 @logger.catch
-def convert(page_xml_path: str):
-    logger.info(f"<= {page_xml_path}")
-    scan_doc = parse_pagexml_file(page_xml_path)
+def convert(page_xml_paths: list[str], output_directory: str = "out"):
+    logger.info(f"<= {typesystem_xml}")
+    with open(typesystem_xml, 'rb') as f:
+        typesystem = load_typesystem(f)
 
-    text, marginalia_ranges, header_range, paragraph_ranges, word_interval_tree = gt.extract_paragraph_text(scan_doc)
+    for page_xml_path in page_xml_paths:
+        logger.info(f"<= {page_xml_path}")
+        scan_doc = parse_pagexml_file(page_xml_path)
 
-    if not text:
-        logger.warning(f"no paragraph text found in {page_xml_path}")
-    else:
-        logger.info(f"<= {typesystem_xml}")
-        with open(typesystem_xml, 'rb') as f:
-            typesystem = load_typesystem(f)
+        text, marginalia_ranges, header_range, paragraph_ranges, word_interval_tree = gt.extract_paragraph_text(scan_doc)
 
-        cas = Cas(typesystem=typesystem)
-        cas.sofa_string = text
-        cas.sofa_mime = "text/plain"
+        if not text:
+            logger.warning(f"no paragraph text found in {page_xml_path}")
+        else:
+            cas = Cas(typesystem=typesystem)
+            cas.sofa_string = text
+            cas.sofa_mime = "text/plain"
 
-        SentenceAnnotation = cas.typesystem.get_type(CAS_SENTENCE)
-        TokenAnnotation = cas.typesystem.get_type(CAS_TOKEN)
-        ParagraphAnnotation = cas.typesystem.get_type(CAS_PARAGRAPH)
-        MarginaliumAnnotation = cas.typesystem.get_type(CAS_MARGINALIUM)
-        HeaderAnnotation = cas.typesystem.get_type(CAS_HEADER)
-        doc = nlp(text)
-        for sentence in doc.sents:
-            cas.add(SentenceAnnotation(begin=sentence.start_char, end=sentence.end_char))
-            for token in [t for t in sentence if t.text != "\n"]:
-                begin = token.idx
-                end = token.idx + len(token.text)
-                cas.add(TokenAnnotation(begin=begin, end=end))
+            SentenceAnnotation = cas.typesystem.get_type(CAS_SENTENCE)
+            TokenAnnotation = cas.typesystem.get_type(CAS_TOKEN)
+            ParagraphAnnotation = cas.typesystem.get_type(CAS_PARAGRAPH)
+            MarginaliumAnnotation = cas.typesystem.get_type(CAS_MARGINALIUM)
+            HeaderAnnotation = cas.typesystem.get_type(CAS_HEADER)
+            doc = nlp(text)
+            for sentence in doc.sents:
+                cas.add(SentenceAnnotation(begin=sentence.start_char, end=sentence.end_char))
+                for token in [t for t in sentence if t.text != "\n"]:
+                    begin = token.idx
+                    end = token.idx + len(token.text)
+                    cas.add(TokenAnnotation(begin=begin, end=end))
 
-        for pr in marginalia_ranges:
-            cas.add(MarginaliumAnnotation(begin=pr[0], end=pr[1]))
-        if header_range:
-            cas.add(HeaderAnnotation(begin=header_range[0], end=header_range[1]))
-        for pr in paragraph_ranges:
-            cas.add(ParagraphAnnotation(begin=pr[0], end=pr[1]))
+            for pr in marginalia_ranges:
+                cas.add(MarginaliumAnnotation(begin=pr[0], end=pr[1]))
+            if header_range:
+                cas.add(HeaderAnnotation(begin=header_range[0], end=header_range[1]))
+            for pr in paragraph_ranges:
+                cas.add(ParagraphAnnotation(begin=pr[0], end=pr[1]))
 
-        # print_annotations(cas)
+            # print_annotations(cas)
 
-        cas_xmi = output_path(page_xml_path)
-        logger.info(f"=> {cas_xmi}")
-        cas.to_xmi(cas_xmi, pretty_print=True)
+            cas_xmi = output_path(page_xml_path, output_directory)
+            logger.info(f"=> {cas_xmi}")
+            cas.to_xmi(cas_xmi, pretty_print=True)
 
 
 if __name__ == '__main__':
     args = get_arguments()
     if args.page_xml_path:
-        convert(args.page_xml_path)
+        convert(args.page_xml_path, args.output_directory)
