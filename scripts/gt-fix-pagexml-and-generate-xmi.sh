bdca4e77987251a418f91220b1bb0486d31389e9
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2025-06-19 09:19:39 +0200

    move files to zip

--- scripts/gt-fix-pagexml-and-generate-xmi.sh
+++ scripts/gt-fix-pagexml-and-generate-xmi.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 INV=$1
+echo "processing $INV..."
 INDIR=~/c/data/globalise/pagexml/$INV
 OUTDIR=out-local/fixed-pagexml/$INV
 mkdir -p $OUTDIR
@@ -7,4 +8,5 @@ cp -a $INDIR/*xml $OUTDIR
 poetry run ./scripts/gt_fix_reading_order2.py -i out-local/fixed-pagexml -o $OUTDIR $INV
 poetry run ./scripts/gt-pagexml-to-uima-cas.py -o $OUTDIR $OUTDIR/*.xml
 chmod a-x $OUTDIR/*.xm?
-(cd out-local/fixed-pagexml && zip -qr $INV-pagexml.zip $INV/*.xml && zip -qr $INV-xmi.zip $INV/*.xmi)
+(cd out-local/fixed-pagexml && zip -qrm $INV-pagexml.zip $INV/*.xml && zip -qrm $INV-xmi.zip $INV/*.xmi)
+echo "done!"
