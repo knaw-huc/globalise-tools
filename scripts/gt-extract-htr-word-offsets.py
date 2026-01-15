6eb55814ff9e108d3246ee1297e75a0e27ef80d0
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2026-01-15 11:14:26 +0100

    make gt-extract-htr-word-offsets.py work with multiple pagexml

--- scripts/gt-extract-htr-word-offsets.py
+++ scripts/gt-extract-htr-word-offsets.py
@@ -15,41 +15,43 @@ def get_arguments() -> Namespace:
     parser = argparse.ArgumentParser(
         description="Extract word offsets from the given pagexml",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-p", "--pagexml",
-                        help="The pagexml file",
-                        type=str
-                        )
     parser.add_argument("-o", "--output-dir",
                         help="The directory to write the offset files to",
                         type=str
                         )
+    parser.add_argument("pagexml_paths",
+                        help="The pagexml file(s)",
+                        type=str,
+                        nargs="+",
+                        )
     return parser.parse_args()
 
 
-def extract_word_offsets(out_dir: str, pagexml_path: str) -> None:
-    try:
-        logger.info(f"<= {pagexml_path}")
-        with open(pagexml_path, "r", encoding="utf-8") as f:
-            xml_string = f.read()
-    except FileNotFoundError:
-        print(f"Input file not found: {pagexml_path}", file=sys.stderr)
-        sys.exit(1)
+def extract_word_offsets(out_dir: str, pagexml_paths: list[str]):
+    for pagexml_path in pagexml_paths:
+        try:
+            logger.info(f"<= {pagexml_path}")
+            with open(pagexml_path, "r", encoding="utf-8") as f:
+                xml_string = f.read()
+        except FileNotFoundError:
+            print(f"Input file not found: {pagexml_path}", file=sys.stderr)
+            sys.exit(1)
 
-    page_id = pagexml_path.split("/")[-1].replace(".xml", "")
+        page_id = pagexml_path.split("/")[-1].replace(".xml", "")
 
-    htr_word_offsets = get_word_offsets(xml_string)
+        htr_word_offsets = get_word_offsets(xml_string)
 
-    out_path = f"{out_dir}/{page_id}.json"
-    logger.info(f"=> {out_path}")
-    with open(out_path, "w", encoding="utf-8") as f:
-        json.dump(htr_word_offsets, f, indent=2, ensure_ascii=False, cls=AnnotationEncoder)
+        out_path = f"{out_dir}/{page_id}.json"
+        logger.info(f"=> {out_path}")
+        with open(out_path, "w", encoding="utf-8") as f:
+            json.dump(htr_word_offsets, f, indent=2, ensure_ascii=False, cls=AnnotationEncoder)
 
 
 @logger.catch
 def main():
     args = get_arguments()
     Path(args.output_dir).mkdir(parents=True, exist_ok=True)
-    extract_word_offsets(args.output_dir, args.pagexml)
+    extract_word_offsets(args.output_dir, args.pagexml_paths)
 
 
 if __name__ == '__main__':
