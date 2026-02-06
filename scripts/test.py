76d9cbc27f09a9532db10fc6059c7e42700efd41
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2026-02-06 23:59:16 +0100

    add script with progressbar

--- scripts/test.py
+++ scripts/test.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
-import json as JSON
-import xml.etree.ElementTree as ET
+
+from gt_ner_xmi_to_wa import load_timespan_dict
+
+
+def load_inv_nrs():
+    path = "data/inventory-numbers.lst"
+    with open(path) as f:
+        return [l.strip() for l in f.readlines()]
 
 
 def main() -> None:
-    file = '../data/NL-HaNA_1.04.02_1092_0017.xml'
-    tree = ET.parse(file)
-    root = tree.getroot()
-
-    json = JSON.loads(root.text)
-    json2 = JSON.loads(json['na_viewer']['view_response'])
-    print(JSON.dumps(json2, indent=4))
+    inv_nrs = load_inv_nrs()
+    timezones = load_timespan_dict()
+    available = timezones.keys()
+    missing = set(inv_nrs) - available
+    if len(missing) > 0:
+        print(f"{len(missing)}/{len(inv_nrs)} Missing timespan:")
+        for n in sorted(missing):
+            print(f" {n}")
 
 
 if __name__ == '__main__':
