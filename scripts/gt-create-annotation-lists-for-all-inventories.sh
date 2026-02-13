a63749e2b94d84d02e2e1b180fd1aa3bb4d502fc
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2026-02-13 15:48:22 +0100

    fix whitespace

--- scripts/gt-create-annotation-lists-for-all-inventories.sh
+++ scripts/gt-create-annotation-lists-for-all-inventories.sh
@@ -6,15 +6,15 @@ EMPTY_CHAR=' '
 start_time=$(date +%s)
 
 fatal() {
-	echo '[FATAL]' "$@" >&2
-	exit 1
+  echo '[FATAL]' "$@" >&2
+  exit 1
 }
 
 progress-bar() {
-	local current=$1
-	local len=$2
+  local current=$1
+  local len=$2
 
-	local perc_done=$((current * 100 / len))
+  local perc_done=$((current * 100 / len))
 
   local current_time=$(date +%s)
   local elapsed_time=$((current_time - start_time))
@@ -22,29 +22,29 @@ progress-bar() {
 
   local dhms=$(seconds-to-dhms $estimated_time)
 
-	local suffix=" $current/$len ($perc_done%) ETR: ${dhms}"
+  local suffix=" $current/$len ($perc_done%) ETR: ${dhms}"
 
-	local length=$((COLUMNS - ${#suffix} - 2))
-	local num_bars=$((perc_done * length / 100))
+  local length=$((COLUMNS - ${#suffix} - 2))
+  local num_bars=$((perc_done * length / 100))
 
-	local i
-	local s='['
-	for ((i = 0; i < num_bars; i++)); do
-		s+=$BAR_CHAR
-	done
-	for ((i = num_bars; i < length; i++)); do
-		s+=$EMPTY_CHAR
-	done
-	s+=']'
-	s+=$suffix
+  local i
+  local s='['
+  for ((i = 0; i < num_bars; i++)); do
+    s+=$BAR_CHAR
+  done
+  for ((i = num_bars; i < length; i++)); do
+    s+=$EMPTY_CHAR
+  done
+  s+=']'
+  s+=$suffix
 
-	printf '\e7' # save the cursor location
-	  printf '\e[%d;%dH' "$LINES" 0 # move cursor to the bottom line
-	  printf '\e[0K' # clear the line
+  printf '\e7' # save the cursor location
+    printf '\e[%d;%dH' "$LINES" 0 # move cursor to the bottom line
+    printf '\e[0K' # clear the line
     echo -ne "\033[34m" # Set color to blue
-	  printf '%s' "$s" # print the progress bar
-	  echo -ne "\033[0m" # Reset color
-	printf '\e8' # restore the cursor location
+    printf '%s' "$s" # print the progress bar
+    echo -ne "\033[0m" # Reset color
+  printf '\e8' # restore the cursor location
 }
 
 seconds-to-dhms() {
@@ -60,48 +60,48 @@ seconds-to-dhms() {
 }
 
 process-inventory() {
-	local invnrs=("$@")
+  local invnrs=("$@")
   make -j $(for i in "${invnrs[@]}"; do echo -n "annotation-lists-$i "; done)
   for invnr in "${invnrs[@]}"; do
-	  echo $invnr >> work/inv-done.lst
-	done
+    echo $invnr >> work/inv-done.lst
+  done
 }
 
 init-term() {
-	printf '\n' # ensure we have space for the scrollbar
-	  printf '\e7' # save the cursor location
-	    printf '\e[%d;%dr' 0 "$((LINES - 1))" # set the scrollable region (margin)
-	  printf '\e8' # restore the cursor location
-	printf '\e[1A' # move cursor up
+  printf '\n' # ensure we have space for the scrollbar
+  printf '\e7' # save the cursor location
+  printf '\e[%d;%dr' 0 "$((LINES - 1))" # set the scrollable region (margin)
+  printf '\e8' # restore the cursor location
+  printf '\e[1A' # move cursor up
 }
 
 deinit-term() {
-	printf '\e7' # save the cursor location
-	  printf '\e[%d;%dr' 0 "$LINES" # reset the scrollable region (margin)
-	  printf '\e[%d;%dH' "$LINES" 0 # move cursor to the bottom line
-	  printf '\e[0K' # clear the line
-	printf '\e8' # reset the cursor location
+  printf '\e7' # save the cursor location
+  printf '\e[%d;%dr' 0 "$LINES" # reset the scrollable region (margin)
+  printf '\e[%d;%dH' "$LINES" 0 # move cursor to the bottom line
+  printf '\e[0K' # clear the line
+  printf '\e8' # reset the cursor location
 }
 
 main() {
-	shopt -s globstar nullglob checkwinsize
-	# this line is to ensure LINES and COLUMNS are set
-	(:)
+  shopt -s globstar nullglob checkwinsize
+  # this line is to ensure LINES and COLUMNS are set
+  (:)
 
-	trap deinit-term exit
-	trap init-term winch
-	init-term
+  trap deinit-term exit
+  trap init-term winch
+  init-term
 
   readarray -t invnrs < work/inv-todo.lst
-	local len=${#invnrs[@]}
-	echo "processing $len inventories"
+  local len=${#invnrs[@]}
+  echo "processing $len inventories"
 
-	local i
-	for ((i = 0; i < len; i += BATCHSIZE)); do
-		progress-bar "$((i+1))" "$len"
-		process-inventory "${invnrs[@]:i:BATCHSIZE}"
-	done
-	progress-bar "$len" "$len"
+  local i
+  for ((i = 0; i < len; i += BATCHSIZE)); do
+    progress-bar "$((i+1))" "$len"
+    process-inventory "${invnrs[@]:i:BATCHSIZE}"
+  done
+  progress-bar "$len" "$len"
 }
 
 main "$@"
