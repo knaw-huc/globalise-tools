fdb4a4246799d55a5a2bda2e941b55f5a812565a
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2026-05-08 16:03:19 +0200

    add poetry script gt-make-globalise-documents-file

---
+++ scripts/gt_make_globalise_documents_file.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import itertools
+import json
+
+from loguru import logger
+
+
+@logger.catch
+def main():
+    path = "data/inventory2dates.json"
+    logger.info(f"<= {path}")
+    with open(path, "r") as f:
+        records = json.load(f)
+    dates4inventory = {r["inventory_number"]: r for r in records}
+
+    path = "data/all-page-ids.lst"
+    logger.info(f"<= {path}")
+    with open(path, "r") as f:
+        page_ids = f.read().splitlines()
+        logger.info(f"read {len(page_ids)} page ids")
+
+    groups = itertools.groupby(page_ids, lambda p: "_".join(p.split("_")[:3]))
+    documents = []
+    for doc_id, page_ids in groups:
+        inventory_number = doc_id.split("_")[-1]
+        doc = dates4inventory[inventory_number]
+        page_id_list = list(page_ids)
+        doc["number_of_pages"] = len(page_id_list)
+        doc["page_ids"] = page_id_list
+        documents.append(doc)
+
+    path = "data/globalise-documents.json"
+    logger.info(f"writing {len(documents)} document definitions")
+    logger.info(f"=> {path}")
+    with open(path, "w") as f:
+        json.dump(documents, f)
+
+
+if __name__ == '__main__':
+    main()
