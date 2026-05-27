2e97b27f0f493634a608b5858234f1d4f9ac7749
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2026-05-27 20:38:32 +0200

    gt-make-entity-linking-csv: get offset mapping from index.json

--- scripts/gt_make_entity_linking_csv.py
+++ scripts/gt_make_entity_linking_csv.py
@@ -17,8 +17,8 @@ class Record(NamedTuple):
     status_id: str = ""
     annotation_entity_type: str = ""
     entity_id: str = ""
-    offset_inventory: str = ""
-    offset_scan: str = ""
+    offset_inventory: int = 0
+    offset_scan: int = 0
     prefix: str = ""
     exact: str = ""
     suffix: str = ""
@@ -39,6 +39,7 @@ class EntityLinkFactory:
         self.document = document
         self.records = []
         self.annotations_parsed = 0
+        self.document_offset = self._read_document_offset_mapping()
 
     def make_entity_link_csv(self) -> None:
         print(f"# inventory number  : {self.inventory_number}")
@@ -57,6 +58,14 @@ class EntityLinkFactory:
             records=self.records
         )
 
+    def _read_document_offset_mapping(self) -> dict[str, int]:
+        inv_index = rw.read_json(f"work/{self.inventory_number}/index.json")
+        mapping = {}
+        for r in inv_index:
+            key = f"{r['page_id']}:{str(r['start_in_page'])}"
+            mapping[key] = r["start_in_doc"]
+        return mapping
+
     def _headers(self) -> list[str]:
         return list(self.records[0]._asdict().keys())
 
@@ -67,45 +76,55 @@ class EntityLinkFactory:
             page = rw.read_json(entities_page_path, quiet=True)
             items = page["items"]
             for annotation in items:
-                self._process_annotation(annotation)
+                self._process_annotation(annotation, page_id)
                 self.annotations_parsed += 1
 
-    def _process_annotation(self, annotation: dict[str, Any]):
+    def _process_annotation(self, annotation: dict[str, Any], page_id: str):
         annotation_id = annotation["id"]
         first_target_selectors = annotation["target"][0]["selector"]
 
         for body in annotation["body"]:
             if body["type"] == "AppellativeStatus":
-                record = self._record_from_appellative_body(body, annotation_id, first_target_selectors)
+                record = self._record_from_appellative_body(body, annotation_id, first_target_selectors, page_id)
                 self.records.append(record)
             elif body["type"] == "ClassificatoryStatus":
-                record = self._record_from_classificatory_body(body, annotation_id, first_target_selectors)
+                record = self._record_from_classificatory_body(body, annotation_id, first_target_selectors, page_id)
                 self.records.append(record)
 
     def _record_from_classificatory_body(
             self,
             body: dict[str, Any],
             annotation_id: str,
-            selectors: list[dict[str, Any]]
+            selectors: list[dict[str, Any]],
+            page_id: str
     ) -> Record:
         subject = body["has_classificatory_subject"]
-        return self._record(annotation_id, body, selectors, subject)
+        return self._record(annotation_id, body, selectors, subject, page_id)
 
     def _record_from_appellative_body(
             self,
             body: dict[str, Any],
             annotation_id: str,
-            selectors: list[dict[str, Any]]
+            selectors: list[dict[str, Any]],
+            page_id: str
     ) -> Record:
         subject = body["has_appellative_subject"]
-        return self._record(annotation_id, body, selectors, subject)
+        return self._record(annotation_id, body, selectors, subject, page_id)
 
-    @staticmethod
-    def _record(annotation_id: str, body: dict[str, Any], selectors: list[dict[str, Any]],
-                subject: dict[str, Any]) -> Record:
+    def _record(
+            self,
+            annotation_id: str,
+            body: dict[str, Any],
+            selectors: list[dict[str, Any]],
+            subject: dict[str, Any],
+            page_id: str
+    ) -> Record:
         text_quote_selector = selectors[0]
         text_position_selector = selectors[1]
-        start = text_position_selector["start"]
+        offset_scan = text_position_selector["start"]
+        key = f"{page_id}:{str(offset_scan)}"
+
+        offset_inventory = self.document_offset[key]
         if "prefix" in text_quote_selector:
             prefix = text_quote_selector["prefix"]
         else:
@@ -119,8 +138,8 @@ class EntityLinkFactory:
             status_id=body["id"],
             annotation_entity_type=subject["type"],
             entity_id=subject["id"],
-            offset_inventory=start,
-            offset_scan=start,
+            offset_inventory=offset_inventory,
+            offset_scan=offset_scan,
             prefix=prefix,
             exact=text_quote_selector["exact"],
             suffix=suffix,
